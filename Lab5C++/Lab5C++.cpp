b1e453b49dcdfa9d8018f24faada387989c2df7a
zloy.goblin.ork@gmail.com <zloy.goblin.ork@gmail.com>
2020-05-06 18:20:59 +0300

    //medium

--- Lab5C++/Lab5C++.cpp
+++ Lab5C++/Lab5C++.cpp
@@ -47,6 +47,31 @@ void EasyLevel(CarOwner *carOwners, const int* k) {
    
 }
 
+void MediumLevel() {
+
+    double *num = new double[10];
+    fstream on("E:\\StructChisla.txt", ios::out | ios::in | ios::trunc);
+    int col = 0,sum = 1;
+    string str;
+    cout << endl <<"Все числа:";
+    while (on >> num[col]) {
+        cout << num[col];
+        col++;
+    }
+    for (int i = 0; i < col;i++) {
+        sum = sum * num[i];
+    }
+    cout << endl << "Квадрат произведения чисел: " << pow(sum, 2);//квадрат произведения чисел 
+    sum = 0;
+
+    for (int i = 0; i < col; i++) {
+        sum = sum + num[i];
+    }
+    cout << endl << "Модуль суммы чисел: " << pow(sum, 2);//модуль суммы чисел 
+   
+    on.close();
+}
+
 int main()
 {
     setlocale(LC_ALL,"");
